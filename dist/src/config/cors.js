421d7313e46de359c653bb7056e09cacc0aeacfd
Antonio Navarro <dojeDulDev@Proton.me>
2025-10-13 12:38:22 +0200

    remove dist folder from repo and to .gitignore

--- dist/src/config/cors.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.corsConfig = void 0;
-exports.corsConfig = {
-    origin: "*"
-};
-//# sourceMappingURL=cors.js.map
